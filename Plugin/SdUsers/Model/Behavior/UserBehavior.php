24e37692f64ea42a7f189524d98aab69c04094be
DERBOIS Aymeric <aymeric@occi-tech.com>
2012-10-17 13:57:45 +0200

    Fix Validator for Users/add in SdUsers/UserBehavior. - refs #2747

--- Plugin/SdUsers/Model/Behavior/UserBehavior.php
+++ Plugin/SdUsers/Model/Behavior/UserBehavior.php
@@ -1,11 +1,11 @@
 <?php
 class UserBehavior extends ModelBehavior {
 
-	protected $_blacklistedRole = array('SuperAdmin', 'Admin', 'Utilisateur');
+	protected $_blacklistedRole = array('3', '4', '5');
 
 	public function beforeValidate(Model $Model) {
 		if ($Model instanceof User) {
-			$Model->validator()->add('role_id', 'inList', array(
+			$Model->validator()->add('role_id', array(
 				'rule' => array('inList', $this->_blacklistedRole),
 				'message' => __('Vous ne pouvez pas créer d\'utilisateur de ce groupe')
 			));
