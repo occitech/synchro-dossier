0cd6333567c19156935e8833f57e6a65fc229081
DERBOIS Aymeric <aymeric@occi-tech.com>
2012-10-18 18:09:57 +0200

    Add permission for FilesController actions - refs #2747

--- Plugin/SdUsers/Config/SdUsersActivation.php
+++ Plugin/SdUsers/Config/SdUsersActivation.php
@@ -13,6 +13,14 @@ class SdUsersActivation {
 
 		$controller->Croogo->addAco('Users/Users/admin_add', array('SdSuperAdmin'));
 		$controller->Croogo->addAco('Users/Users/admin_index', array('SdSuperAdmin'));
+		$controller->Croogo->addAco('Users/Users/admin_edit', array('SdSuperAdmin'));
+
+		$controller->Croogo->addAco('Uploader/Files/browse', array('SdSuperAdmin', 'sdUtilisateur', 'sdAdmin'));
+		$controller->Croogo->addAco('Uploader/Files/upload', array('SdSuperAdmin', 'sdUtilisateur', 'sdAdmin'));
+		$controller->Croogo->addAco('Uploader/Files/download', array('SdSuperAdmin', 'sdUtilisateur', 'sdAdmin'));
+		$controller->Croogo->addAco('Uploader/Files/rename', array('SdSuperAdmin', 'sdUtilisateur', 'sdAdmin'));
+		$controller->Croogo->addAco('Uploader/Files/createFolder', array('SdSuperAdmin', 'sdUtilisateur', 'sdAdmin'));
+		$controller->Croogo->addAco('Uploader/Files/downloadZipFolder', array('SdSuperAdmin', 'sdUtilisateur', 'sdAdmin'));
 	}
 
 	public function beforeDeactivation(&$controller) {
@@ -25,5 +33,13 @@ class SdUsersActivation {
 
 		$controller->Croogo->removeAco('Users/Users/admin_add', array('SdSuperAdmin'));
 		$controller->Croogo->removeAco('Users/Users/admin_index', array('SdSuperAdmin'));
+		$controller->Croogo->removeAco('Users/Users/admin_edit', array('SdSuperAdmin'));
+
+		$controller->Croogo->removeAco('Uploader/Files/browse', array('SdSuperAdmin', 'sdUtilisateur', 'sdAdmin'));
+		$controller->Croogo->removeAco('Uploader/Files/upload', array('SdSuperAdmin', 'sdUtilisateur', 'sdAdmin'));
+		$controller->Croogo->removeAco('Uploader/Files/download', array('SdSuperAdmin', 'sdUtilisateur', 'sdAdmin'));
+		$controller->Croogo->removeAco('Uploader/Files/rename', array('SdSuperAdmin', 'sdUtilisateur', 'sdAdmin'));
+		$controller->Croogo->removeAco('Uploader/Files/createFolder', array('SdSuperAdmin', 'sdUtilisateur', 'sdAdmin'));
+		$controller->Croogo->removeAco('Uploader/Files/downloadZipFolder', array('SdSuperAdmin', 'sdUtilisateur', 'sdAdmin'));
 	}
 }
