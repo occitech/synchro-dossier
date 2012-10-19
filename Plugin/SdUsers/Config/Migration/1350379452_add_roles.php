b09a4fdcb716af667b83c543cf5365d4dc1e6076
DERBOIS Aymeric <aymeric@occi-tech.com>
2012-10-19 15:30:03 +0200

    Refactoring of migration file "add_profil" - refs #2747

--- Plugin/SdUsers/Config/Migration/1350379452_add_roles.php
+++ Plugin/SdUsers/Config/Migration/1350379452_add_roles.php
@@ -4,103 +4,79 @@ App::uses('ClassRegistry', 'Utility');
 
 class AddRoles extends CakeMigration {
 
-/**
- * Migration description
- *
- * @var string
- * @access public
- */
 	public $description = '';
 
-/**
- * Actions to be performed
- *
- * @var array $migration
- * @access public
- */
 	public $migration = array(
 		'up' => array(),
 		'down' => array(),
 	);
 
-/**
- * Before migration callback
- *
- * @param string $direction, up or down direction of migration process
- * @return boolean Should process continue
- * @access public
- */
+	private $__roles = array(
+		array('id' => '4', 'title' => 'SuperAdmin', 'alias' => 'sdSuperAdmin'),
+		array('id' => '5', 'title' => 'Admin', 'alias' => 'sdAdmin'),
+		array('id' => '6', 'title' => 'Utilisateur', 'alias' => 'sdUtilisateur')
+	);
+
+	private $__links = array(
+		array(
+			'menu_id' => 3,
+			'title' => 'Accueil',
+			'class' => 'accueil',
+			'link' => '/',
+			'visibility_roles' => '["1","4","5","6"]'
+		),
+		array(
+			'menu_id' => 3,
+			'title' => 'Ajouter un utilisateur',
+			'class' => 'add-user',
+			'link' => 'admin:true/plugin:users/controller:users/action:add',
+			'visibility_roles' => '["1","4"]'
+		)
+	);
+
+	private function __renameAdminRole($name) {
+		return $this->Role->updateAll(
+			array('Role.title' => "'$name'"),
+			array('Role.alias' => 'admin')
+		);
+	}
+
+	private function __addSynchroRoles() {
+		return $this->Role->saveMany($this->__roles, array('deep' => true));
+	}
+
+	private function __removeAllMainMenuLink() {
+		return $this->Link->deleteAll(array('menu_id' => 3));
+	}
+
+	private function __addNewMainMenuLink() {
+		return $this->Link->saveMany($links, array('deep' => true));
+	}
+
+	private function __removeSynchroRoles() {
+		return $this->Role->deleteAll(array('alias' => Hash::extract($this->__roles, '{n}.alias'), true, true));
+	}
+
 	public function before($direction) {
 		return true;
 	}
 
-/**
- * After migration callback
- *
- * @param string $direction, up or down direction of migration process
- * @return boolean Should process continue
- * @access public
- */
 	public function after($direction) {
+		$this->Role = ClassRegistry::init('Users.Role');
+		$this->Link = ClassRegistry::init('Menus.Link');
+		$success = true;
+
 		if ($direction == 'up') {
-			$this->_afterUp();
+			$success = 
+				$this->__renameAdminRole('Occitech') &&
+				$this->__addSynchroRoles() &&
+				$this->__removeAllMainMenuLink() &&
+				$this->__addNewMainMenuLink();
 		} else {
-			$this->_afterDown();
+			$success =
+				$this->__removeSynchroRoles() &&
+				$this->__renameAdminRole('Admin');
 		}
-		return true;
-	}
-
-	protected function _afterUp() {
-		$roleModel = ClassRegistry::init('Users.Role');
-		$linkModel = ClassRegistry::init('Menus.Link');
-		
-		// Change Admin by Occitech
-		$roleModel->updateAll(
-			array('Role.title' => "'Occitech'"),
-			array('Role.alias' => 'admin')
-		);
-
-		// Add 3 new roles 
-		$roles = array(
-			array('id' => '4', 'title' => 'SuperAdmin', 'alias' => 'sdSuperAdmin'),
-			array('id' => '5', 'title' => 'Admin', 'alias' => 'sdAdmin'),
-			array('id' => '6', 'title' => 'Utilisateur', 'alias' => 'sdUtilisateur')
-		);
-		$roleModel->saveMany($roles, array('deep' => true));
-
-		// Add link to add user in menu
-		$linkModel->deleteAll(array('menu_id' => 3));
-		$links = array(
-			array(
-				'menu_id' => 3,
-				'title' => 'Accueil',
-				'class' => 'accueil',
-				'link' => '/',
-				'visibility_roles' => '["1","4","5","6"]'
-			),
-			array(
-				'menu_id' => 3,
-				'title' => 'Ajouter un utilisateur',
-				'class' => 'add-user',
-				'link' => 'admin:true/plugin:users/controller:users/action:add',
-				'visibility_roles' => '["1","4"]'
-			)
-		);
-		$linkModel->saveMany($links, array('deep' => true));
-	}
-
-	protected function _afterDown() {
-		$roleModel = ClassRegistry::init('Users.Role');
-
-		// Remove 3 roles
-		$roleModel->deleteAll(array('alias' => 'sdSuperAdmin'), true, true);
-		$roleModel->deleteAll(array('alias' => 'sdAdmin'), true, true);
-		$roleModel->deleteAll(array('alias' => 'sdUtilisateur'), true, true);
-
-		// Change Occitech by Admin
-		$roleModel->updateAll(
-			array('Role.title' => "'Admin'"),
-			array('Role.alias' => 'admin')
-		);
+		return $success;
 	}
 }
