356146fb59aed9b533204a4c6646d5b4b1d4f52b
DERBOIS Aymeric <aymeric@occi-tech.com>
2012-10-25 12:40:01 +0200

    User can be edited - refs #2759

--- Plugin/SdUsers/Controller/SdUsersController.php
+++ Plugin/SdUsers/Controller/SdUsersController.php
@@ -12,6 +12,21 @@ class SdUsersController extends SdUsersAppController {
 		} else {
 			$this->paginate = $this->SdUser->getPaginateAll();
 		}
+		// debug($this->paginate('SdUser'));
 		$this->set('users', $this->paginate('SdUser'));
+	}
+
+	public function admin_edit($userId) {
+		if ($this->request->data) {
+			if ($this->SdUser->saveAssociated($this->request->data)) {
+				$this->Session->setFlash(__('The User has been saved'), 'default', array('class' => 'success'));
+				$this->redirect(array('action' => 'index'));
+			} else {
+				$this->Session->setFlash(__('The User could not be saved. Please, try again.'), 'default', array('class' => 'error'));
+			}
+		} else {
+			$this->request->data = $this->SdUser->read(null, $userId);
+		}
+		$this->set('roles', $this->SdUser->Role->find('list'));
 	}
 }
