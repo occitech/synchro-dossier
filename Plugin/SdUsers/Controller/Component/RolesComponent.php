c5c016a80ee9fef2d7d48b273d3aa5a0f02b7126
DERBOIS Aymeric <aymeric@occi-tech.com>
2012-10-25 16:16:49 +0200

    List of role on admin_add or admin_edit depend of the creator role - refs #2759

--- Plugin/SdUsers/Controller/Component/RolesComponent.php
+++ Plugin/SdUsers/Controller/Component/RolesComponent.php
@@ -4,12 +4,14 @@ App::uses('Component', 'Controller');
 
 class RolesComponent extends Component {
 
-	protected $_blacklistedRole = array('Occitech', 'Public', 'Registered');
-
 	public function beforeRender(&$Controller) {
 		if ($Controller instanceof SdUsersController) {
+			// Change the role list in function of user role
 			if (in_array($Controller->action, array('admin_add', 'admin_edit'))) {
-				$Controller->viewVars['roles'] = array_diff($Controller->viewVars['roles'], $this->_blacklistedRole);
+				$roleId = $Controller->Auth->user('role_id');
+				$whiteListRole = Configure::read('sd.' . $roleId . '.authorizeRoleCreation');
+				$whiteListRole = array_flip($whiteListRole);
+				$Controller->viewVars['roles'] = array_intersect_key($Controller->viewVars['roles'], $whiteListRole);
 			}
 		}
 	}
