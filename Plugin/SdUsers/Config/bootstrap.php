d20c9f210910b14655fc3ab94dfacd8f53fac9af
DERBOIS Aymeric <aymeric@occi-tech.com>
2012-10-25 10:58:12 +0200

    Add information about role (id and alias) in Configure::write - refs #2759

--- Plugin/SdUsers/Config/bootstrap.php
+++ Plugin/SdUsers/Config/bootstrap.php
@@ -5,3 +5,13 @@ Croogo::hookBehavior('User', 'SdUsers.User');
 
 Croogo::hookAdminTab('Users/admin_add', 'Profil', 'sdUsers.admin_tab_user');
 Croogo::hookAdminTab('Users/admin_edit', 'Profil', 'sdUsers.admin_tab_user');
+
+// Information about Sd roles
+Configure::write('sd.SuperAdmin.roleId', 4);
+Configure::write('sd.SuperAdmin.roleAlias', 'sdSuperAdmin');
+
+Configure::write('sd.Admin.roleId', 5);
+Configure::write('sd.Admin.roleAlias', 'sdAdmin');
+
+Configure::write('sd.Utilisateur.roleId', 6);
+Configure::write('sd.Utilisateur.roleAlias', 'sdUtilisateur');
