86bb51267ee708351c9fc7274ad5756f7115771a
DERBOIS Aymeric <aymeric@occi-tech.com>
2012-10-25 12:11:42 +0200

    Add creator_id field in users tables (that fix bug in last commits) - refs #2759

---
+++ Plugin/SdUsers/Config/Migration/1351159702_add_creator_id.php
@@ -0,0 +1,54 @@
+<?php
+class AddCreatorId extends CakeMigration {
+
+/**
+ * Migration description
+ *
+ * @var string
+ * @access public
+ */
+	public $description = '';
+
+/**
+ * Actions to be performed
+ *
+ * @var array $migration
+ * @access public
+ */
+	public $migration = array(
+		'up' => array(
+			'create_field' => array(
+				'users' => array(
+					'creator_id' => array('type' => 'integer', 'null' => false, 'default' => NULL, 'after' => 'role_id'),
+				),
+			),
+		),
+		'down' => array(
+			'drop_field' => array(
+				'users' => array('creator_id',),
+			),
+		),
+	);
+
+/**
+ * Before migration callback
+ *
+ * @param string $direction, up or down direction of migration process
+ * @return boolean Should process continue
+ * @access public
+ */
+	public function before($direction) {
+		return true;
+	}
+
+/**
+ * After migration callback
+ *
+ * @param string $direction, up or down direction of migration process
+ * @return boolean Should process continue
+ * @access public
+ */
+	public function after($direction) {
+		return true;
+	}
+}
