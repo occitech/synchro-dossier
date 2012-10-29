2939dbbe93229af1af70c4ff82474e64eff3513c
DERBOIS Aymeric <aymeric@occi-tech.com>
2012-10-29 15:36:03 +0100

    Refactoring of RolesComponent. beforeRender splited in two methods - refs #2747

--- Plugin/SdUsers/Controller/Component/RolesComponent.php
+++ Plugin/SdUsers/Controller/Component/RolesComponent.php
@@ -4,14 +4,17 @@ App::uses('Component', 'Controller');
 
 class RolesComponent extends Component {
 
+	protected function __restrictAllowedRoles(&$Controller) {
+		$roleId = $Controller->Auth->user('role_id');
+		$whiteListRole = Configure::read('sd.' . $roleId . '.authorizeRoleCreation');
+		$whiteListRole = array_flip($whiteListRole);
+		$Controller->viewVars['roles'] = array_intersect_key($Controller->viewVars['roles'], $whiteListRole);
+	}
+
 	public function beforeRender(&$Controller) {
 		if ($Controller instanceof SdUsersController) {
-			// Change the role list in function of user role
 			if (in_array($Controller->action, array('admin_add', 'admin_edit'))) {
-				$roleId = $Controller->Auth->user('role_id');
-				$whiteListRole = Configure::read('sd.' . $roleId . '.authorizeRoleCreation');
-				$whiteListRole = array_flip($whiteListRole);
-				$Controller->viewVars['roles'] = array_intersect_key($Controller->viewVars['roles'], $whiteListRole);
+				$this->__restrictAllowedRoles($Controller);
 			}
 		}
 	}
