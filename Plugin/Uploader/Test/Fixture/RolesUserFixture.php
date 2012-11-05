836cf19005ddc946b01b1faa9bcd1c2aa17bd2b4
DERBOIS Aymeric <aymeric@occi-tech.com>
2012-11-05 16:15:09 +0100

    Model "AclAco" added in Uploader plugin. - refs #3021

---
+++ Plugin/Uploader/Test/Fixture/RolesUserFixture.php
@@ -0,0 +1,35 @@
+<?php
+/**
+ * RolesUserFixture
+ *
+ */
+class RolesUserFixture extends CakeTestFixture {
+
+/**
+ * Fields
+ *
+ * @var array
+ */
+	public $fields = array(
+		'id' => array('type' => 'integer', 'null' => false, 'default' => null, 'key' => 'primary'),
+		'user_id' => array('type' => 'integer', 'null' => false, 'default' => null, 'key' => 'index'),
+		'role_id' => array('type' => 'integer', 'null' => false, 'default' => null),
+		'granted_by' => array('type' => 'integer', 'null' => false, 'default' => null),
+		'created' => array('type' => 'datetime', 'null' => true, 'default' => null),
+		'updated' => array('type' => 'datetime', 'null' => true, 'default' => null),
+		'indexes' => array(
+			'PRIMARY' => array('column' => 'id', 'unique' => 1),
+			'pk_role_users' => array('column' => array('user_id', 'role_id'), 'unique' => 1)
+		),
+		'tableParameters' => array('charset' => 'utf8', 'collate' => 'utf8_unicode_ci', 'engine' => 'MyISAM')
+	);
+
+/**
+ * Records
+ *
+ * @var array
+ */
+	public $records = array(
+	);
+
+}
