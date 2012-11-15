859dd075ddfebefade91f1aeb73a8d94d02bb89a
DERBOIS Aymeric <aymeric@occi-tech.com>
2012-11-15 13:12:36 +0100

    Admin menu for Quota created - refs #2745

---
+++ Plugin/SynchroDossier/Config/bootstrap.php
@@ -0,0 +1,23 @@
+<?php
+
+$adminMenu = array(
+	'title' => __('SynchroDossier'),
+	'url' => array(
+		'admin' => true,
+		'plugin' => 'synchro_dossier',
+		'controller' => 'sd_informations',
+		'action' => 'index',
+	),
+	'children' => array(
+		'list' => array(
+			'title' => __('Quota'),
+			'url' => array(
+				'admin' => true,
+				'plugin' => 'synchro_dossier',
+				'controller' => 'sd_informations',
+				'action' => 'quota',
+			),
+		),
+	),
+);
+CroogoNav::add('synchro', $adminMenu);
