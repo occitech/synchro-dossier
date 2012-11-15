fc840887a672a6f015994bf63feb732a7da460b2
DERBOIS Aymeric <aymeric@occi-tech.com>
2012-11-15 14:17:17 +0100

    Add validation for quota - refs #2745

--- Plugin/SynchroDossier/Model/SdInformation.php
+++ Plugin/SynchroDossier/Model/SdInformation.php
@@ -5,6 +5,16 @@ App::uses('SynchroDossierAppModel', 'SynchroDossier.Model');
 class SdInformation extends SynchroDossierAppModel {
 	public $useTable = 'sd_information';
 
+	public $validate = array(
+		'quota_mb' => array(
+			'comparison' => array(
+				'rule' => array('comparison', '>', 0),
+				'message' => 'Quota must be highter than 0',
+				'required' => true
+			),
+		),
+	);
+
 	public function getUsedQuota() {
 		$data = $this->find('first');
 
