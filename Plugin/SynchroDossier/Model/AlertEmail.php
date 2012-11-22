c8614f35ff33b6b0867c1d0d82a1b547636f353a
DERBOIS Aymeric <aymeric@occi-tech.com>
2012-11-22 13:23:53 +0100

    add table alert_emails - refs #3030

---
+++ Plugin/SynchroDossier/Model/AlertEmail.php
@@ -0,0 +1,17 @@
+<?php
+
+App::uses('SynchroDossierAppModel', 'SynchroDossier.Model');
+
+class AlertEmail extends SynchroDossierAppModel {
+
+	public $belongsTo = array(
+		'User' => array(
+			'className' => 'SdUsers.SdUser',
+			'foreignKey' => 'user_id',
+		),
+		'UploadedFile' => array(
+			'className' => 'Uploader.UploadedFile',
+			'foreignKey' => 'uploaded_file_id',
+		)
+	);
+}
