427c182c3d7e251e61975bf2e13713b284ad54fa
DERBOIS Aymeric <aymeric@occi-tech.com>
2012-11-29 12:37:45 +0100

    Add uploader_widget js, css and element - refs #3115

---
+++ Plugin/Uploader/View/Elements/plupload_widget.ctp
@@ -0,0 +1,39 @@
+<?php $this->Html->css('Uploader.uploader_widget', null, array('inline' => false)); ?>
+<?php $this->Html->script('Uploader.uploader_widget', array('inline' => false)); ?>
+
+<?php $this->Html->scriptStart(array('inline' => false)); ?>
+	uploaderWidget.init(
+		<?php echo $this->Plupload->getOptions();?>,
+		'#nbFiles', '#nbFilesToSend', '#totalSize', '.send', '.plupload-bar', '.popover-upload'
+	);
+<?php $this->Html->scriptEnd(); ?>
+
+<div class="row-fluid" id="plupload">
+	<div id="drop-area">
+		<p><?= __('Glisser/Déposer vos fichier ici'); ?></p>
+		<span>ou</span>
+		<a href="#" id="browse" class="btn"><?= __('Parcourir'); ?></a>
+	
+		<div class="progress progress-striped active">
+			<div class="bar plupload-bar" style="width: 0%;"></div>
+		</div>
+	</div>
+</div>
+<div class="file-list">
+	<div class="popover fade bottom in popover-upload">
+		<div class="arrow"></div>
+		<div class="popover-inner">
+			<h3 class="popover-title"><?= __('Fichier dans la file d\'attente'); ?></h3>
+			<div class="popover-content">
+				<div class="list">
+
+				</div>
+				<p class="upload-send"><a href="#" class="btn send"><?= __('Envoyer'); ?></a></p>
+			</div>
+		</div>
+	</div>
+</div>
+
+<div style="width: 100%; text-align: center;">
+	
+</div>
---
+++ Plugin/Uploader/webroot/js/uploader_widget.js
@@ -0,0 +1,72 @@
+uploaderWidget = function() {
+	var __options, __nbFilesElt, __nbFilesToSendElt, __filesListElt,
+		__totalSizeElt, __sendButtonElt, __progressBarElt;
+
+	var __nbFiles = 0, __nbFilesToSend = 0, __totalSize = 0;
+
+	function init(options, nbFiles, nbFilesToSend, totalSize, sendButton, progressBar, filesList) {
+		__options = options;
+		__nbFilesElt = nbFiles;
+		__nbFilesToSendElt = nbFilesToSend;
+		__totalSizeElt = totalSize;
+		__sendButtonElt = sendButton;
+		__progressBarElt = progressBar;
+		__filesListElt = filesList;
+		__nameLength = 25;
+
+		uploader = new plupload.Uploader(options);
+
+		uploader.init();
+
+		$(__sendButtonElt).click(function() {
+			$(__progressBarElt).css('width', '0%');
+			$(__filesListElt).css('display', 'none');
+			uploader.start();
+			uploader.refresh();
+		});
+
+		uploader.bind('FilesAdded', function(up, files) {
+			$(__filesListElt).css('display', 'block');
+			for (var i in files) {
+				__totalSize += files[i].size;
+				__nbFiles++;
+				__nbFilesToSend++;
+				var content = $(__filesListElt).find('.list');
+				content.prepend('<p>' + 
+					__truncate(files[i].name, __nameLength) + ' (' + 
+					plupload.formatSize(files[i].size) + ')</p>'
+				);
+			}
+		});
+
+		uploader.bind('UploadProgress', function(up, file, response) {
+			$(__progressBarElt).css('width', up.total.percent + '%');
+		});
+
+		uploader.bind('FileUploaded', function(up, files) {
+			__nbFilesToSend--;
+			$(__nbFilesToSendElt).text(__nbFilesToSend);
+		});
+
+		uploader.bind('UploadComplete', function(up, files) {
+			__totalSize = 0;
+			__nbFiles = 0;
+			__nbFilesToSend = 0;
+			$(__filesListElt).css('display', 'none');
+			$(__filesListElt).find('.list').text('');
+			location.assign(location.href);
+			location.reload();
+		});
+	}
+
+	function __truncate (str, length) {
+		if (str.length > length) {
+			str = str.substring(0, length) + '...';
+		}
+		return str;
+	}
+
+	return {
+		init: init
+	}
+}();
