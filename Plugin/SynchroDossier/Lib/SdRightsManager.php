a0bcfa6d00730b4c4ddfb02e4c76a22e4e253157
DERBOIS Aymeric <aymeric@occi-tech.com>
2012-12-10 16:40:56 +0100

    Optimize and rename method that give all rights to Admin on a folder - refs #3115

--- Plugin/SynchroDossier/Lib/SdRightsManager.php
+++ Plugin/SynchroDossier/Lib/SdRightsManager.php
@@ -14,11 +14,11 @@ class SdRightsManager implements CakeEventListener {
 
 	public function implementedEvents() {
 		return array(
-			'Controller.FilesController.afterChangeRight' => 'updateRights'
+			'Controller.FilesController.afterChangeRight' => 'setAdminAllRights'
 		);
 	}
 
-	public function updateRights($event) {
+	public function setAdminAllRights($event) {
 		if ($event->data['method'] == '_allowRight') {
 			$SdUserModel = ClassRegistry::init('SdUsers.SdUser');
 			$user = $SdUserModel->findById($event->data['user']['id']);
@@ -26,9 +26,12 @@ class SdRightsManager implements CakeEventListener {
 			if ($user['User']['role_id'] == Configure::read('sd.Admin.roleId')) {
 				$PermissionModel = ClassRegistry::init('Permission');
 				$PermissionModel->id = $event->data['foreign_key'];
-				$PermissionModel->saveField('_create', 1);
-				$PermissionModel->saveField('_delete', 1);
-				$PermissionModel->saveField('_change_right', 1);
+				$data = array(
+					'_create' => 1,
+					'_delete' => 1,
+					'_change_right' => 1
+				);
+				$PermissionModel->save($data);
 			}
 		}
 	}
