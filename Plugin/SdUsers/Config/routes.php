227f01b6e70a1f05558fe3807356528be307fd07
DERBOIS Aymeric <aymeric@occi-tech.com>
2012-12-18 12:50:18 +0100

    Admin login route on user login

--- Plugin/SdUsers/Config/routes.php
+++ Plugin/SdUsers/Config/routes.php
@@ -3,3 +3,9 @@
 CroogoRouter::connect('/users/login', array(
 	'plugin' => 'users', 'controller' => 'users', 'action' => 'login'
 ));
+
+CroogoRouter::connect('/admin/users/users/login', array(
+	'plugin' => 'users', 'controller' => 'users', 'action' => 'login'
+));
+
+Router::promote();
