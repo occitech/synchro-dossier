a5f11800a203847ea6e9c6b891239af4cb5f5dd6
gregory <gregory@occi-tech.com>
2013-04-04 13:09:04 +0200

    uses users instead of sdusers as base path

--- Plugin/SdUsers/Config/routes.php
+++ Plugin/SdUsers/Config/routes.php
@@ -4,14 +4,14 @@ CroogoRouter::connect('/users/login', array(
 	'plugin' => 'users', 'controller' => 'users', 'action' => 'login'
 ));
 
-CroogoRouter::connect('/sdusers', array(
+CroogoRouter::connect('/users', array(
 	'plugin' => 'sd_users', 'controller' => 'sd_users', 'action' => 'index'
 ));
 
-CroogoRouter::connect('/sdusers/edit', array(
+CroogoRouter::connect('/users/edit', array(
 	'plugin' => 'sd_users', 'controller' => 'sd_users', 'action' => 'edit'
 ));
 
-CroogoRouter::connect('/sdusers/add', array(
+CroogoRouter::connect('/users/add', array(
 	'plugin' => 'sd_users', 'controller' => 'sd_users', 'action' => 'add'
 ));
