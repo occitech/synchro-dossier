b4ded98e84134a1f052fbcccf4b8457500d9e1a4
Damien Biasotto <damien@occi-tech.com>
2013-05-14 19:06:45 +0200

    Fix empty Users usernames already registered.  closes #3911

---
+++ Config/Migration/1368546294_fix_empty_username.php
@@ -0,0 +1,69 @@
+<?php
+class FixEmptyUsername extends CakeMigration {
+
+/**
+ * Migration description
+ *
+ * @var string
+ * @access public
+ */
+	public $description = 'This migration is irreversible. Down direction will have no effect';
+
+/**
+ * Actions to be performed
+ *
+ * @var array $migration
+ * @access public
+ */
+	public $migration = array(
+		'up' => array(
+		),
+		'down' => array(
+		),
+	);
+
+/**
+ * Before migration callback
+ *
+ * @param string $direction, up or down direction of migration process
+ * @return boolean Should process continue
+ * @access public
+ */
+	public function before($direction) {
+		$success = true;
+
+		if ($direction == 'up') {
+			$User = ClassRegistry::init('SdUsers.SdUser');
+			$users = $User->find('all', array(
+				'conditions' => array(
+					'username' => ''
+				),
+				'contain' => array('Profile'),
+				'noRoleChecking' => true
+			));
+
+			foreach ($users as &$user) {
+				$user['User']['username'] = $this->_generateUsername($user['Profile']);
+			}
+
+			$success = $success && $User->saveMany($users);
+		}
+
+		return $success;
+	}
+
+/**
+ * After migration callback
+ *
+ * @param string $direction, up or down direction of migration process
+ * @return boolean Should process continue
+ * @access public
+ */
+	public function after($direction) {
+		return true;
+	}
+
+	protected function _generateUsername($profileData) {
+		return strtolower(sprintf('%s.%s', $profileData['name'], $profileData['firstname']));
+	}
+}
