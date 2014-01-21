5ce2bca5b3ec7952dedb27e1946fd26d6b356c6c
Hadrien <hadrien@occitech.fr>
2014-01-21 17:59:58 +0100

    Created an all synchro dossier test suite - refs #5325

---
+++ Test/Case/AllSdTest.php
@@ -0,0 +1,19 @@
+<?php
+
+class AllTerrancleTest extends CakeTestSuite {
+
+	public $label = 'Synchro Dossier - All tests';
+
+	public static function suite() {
+		$suite = new CakeTestSuite('Synchro Dossier - All tests');
+
+		$suite->addTestDirectoryRecursive(TESTS . 'Case' . DS . 'Lib');
+		$_pluginTests = array('SdLogs', 'SdUsers', 'Uploader', 'SynchroDossier');
+		foreach ($_pluginTests as $name) {
+			$suite->addTestDirectoryRecursive(App::pluginPath($name) . 'Test' . DS . 'Case');
+		}
+
+		return $suite;
+	}
+
+}
