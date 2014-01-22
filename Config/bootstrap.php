32f51841a85bf57d0fc7d8e74f554627d06ed825
Hadrien <hadrien@occitech.fr>
2014-01-22 16:56:11 +0100

    Added composer for phpunit dependency

--- Config/bootstrap.php
+++ Config/bootstrap.php
@@ -1,4 +1,13 @@
 <?php
+// Load Composer autoload.
+require APP . '/Vendor/autoload.php';
+
+// Remove and re-prepend CakePHP's autoloader as Composer thinks it is the
+// most important.
+// See: http://goo.gl/kKVJO7
+spl_autoload_unregister(array('App', 'load'));
+spl_autoload_register(array('App', 'load'), true, true);
+
 /**
  * This file is loaded automatically by the app/webroot/index.php file after the core bootstrap.php
  *
